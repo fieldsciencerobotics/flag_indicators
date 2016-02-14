4ca42577d7b7667448e764c25045ea582f878970
Jamie Diprose <jamie.diprose@gmail.com>
2016-02-14 19:25:37 +1300

    update

--- Flag.cpp
+++ Flag.cpp
@@ -1,3 +1,27 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Field Science Robotics
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "Flag.h"
 #include "HardwareSerial.h"
 
@@ -17,9 +41,7 @@ void Flag::attach(int pin)
 }
 
 void Flag::lower(float seconds)
-{   
- // Serial.println("Pushing down");
-  
+{     
   int cycles = 50 * seconds;
   for(int i = 0; i < cycles; i++)
   {    
@@ -32,9 +54,7 @@ void Flag::lower(float seconds)
 }
   
 void Flag::raise(float seconds)
-{
- // Serial.println("Pulling up");
-  
+{  
   int cycles = 50 * seconds;
   for(int i = 0; i < cycles; i++)
   {    
